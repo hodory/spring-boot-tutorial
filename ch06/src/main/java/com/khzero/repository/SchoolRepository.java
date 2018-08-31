6910ad547077f87786eca4f564c7678518d4da99
김호영 <khyoung@waug.com>
2018-08-31 16:27:51 +0900

    [6.2.3] ORM 도구와 활용 - 연관 관계

---
+++ ch06/src/main/java/com/khzero/repository/SchoolRepository.java
@@ -0,0 +1,9 @@
+package com.khzero.repository;
+
+import com.khzero.model.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
+}
---
+++ ch06/src/main/java/com/khzero/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.khzero.repository;
+
+import com.khzero.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
